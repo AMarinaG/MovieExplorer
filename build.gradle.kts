de77b03d5f946c2f6229a89dbac4b548f265ac99
AMarinaG <alberto.marina.gonzalo@gmail.com>
2023-06-22 15:52:26 +0200

    feat: added plugin secret and ACCESS_TOKEN_AUTH default

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,5 +12,6 @@ plugins {
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.secrets) apply false
 }
 true // Needed to make the Suppress annotation work for the plugins block
