391c9fe6897e7119e1c10f8cca669c3ad6675215
AMarinaG <alberto.marina.gonzalo@gmail.com>
2023-06-23 00:40:34 +0200

    feat: create basic BottomBar

---
+++ app/src/main/java/com/amarinag/movieexplorer/navigation/TopLevelDestination.kt
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.amarinag.movieexplorer.navigation
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.ArtTrack
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material.icons.outlined.StarOutline
+import androidx.compose.material.icons.outlined.Upcoming
+import androidx.compose.material.icons.rounded.ArtTrack
+import androidx.compose.material.icons.rounded.Person
+import androidx.compose.material.icons.rounded.Star
+import androidx.compose.material.icons.rounded.Upcoming
+import androidx.compose.ui.graphics.vector.ImageVector
+import com.amarinag.movieexplorer.R
+
+enum class TopLevelDestination(
+    val selectedIcon: ImageVector,
+    val unselectedIcon: ImageVector,
+    val iconTextId: Int,
+    val titleTextId: Int,
+) {
+    NOW_PLAYING(
+        selectedIcon = Icons.Rounded.ArtTrack,
+        unselectedIcon = Icons.Outlined.ArtTrack,
+        iconTextId = R.string.app_name,
+        titleTextId = R.string.app_name
+
+    ),
+    FAVORITE(
+        selectedIcon = Icons.Rounded.Star,
+        unselectedIcon = Icons.Outlined.StarOutline,
+        iconTextId = R.string.app_name,
+        titleTextId = R.string.app_name
+    )
+}
--- app/src/main/java/com/amarinag/movieexplorer/ui/MovieApp.kt
+++ app/src/main/java/com/amarinag/movieexplorer/ui/MovieApp.kt
@@ -3,20 +3,103 @@ package com.amarinag.movieexplorer.ui
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navOptions
+import com.amarinag.feature.favorite.navigation.navigateToFavorite
+import com.amarinag.feature.nowplaying.navigation.navigateToNowPlaying
 import com.amarinag.movieexplorer.navigation.MovieNavHost
+import com.amarinag.movieexplorer.navigation.TopLevelDestination
 
 @Composable
 fun MovieApp() {
     val navController = rememberNavController()
     Scaffold(
         modifier = Modifier.fillMaxSize(),
+        bottomBar = {
+            MovieBottomBar(
+                TopLevelDestination.values().toList(),
+                { navController.navigateToTopLevelDestination(it) },
+                navController.currentDestination()
+            )
+        }
     ) { padding ->
         Column(Modifier.padding(padding)) {
             MovieNavHost(navController = navController)
         }
     }
 }
+
+@Composable
+private fun MovieBottomBar(
+    destinations: List<TopLevelDestination>,
+    onNavigateToDestination: (TopLevelDestination) -> Unit,
+    currentDestination: NavDestination?,
+    modifier: Modifier = Modifier,
+) {
+    NavigationBar(
+        modifier = modifier,
+        tonalElevation = 0.dp
+    ) {
+        destinations.forEach { destination ->
+            val selected = currentDestination.isTopLevelDestinationInHierarchy(destination)
+            NavigationBarItem(
+                selected = selected,
+                onClick = { onNavigateToDestination(destination) },
+                icon = {
+                    if (selected) {
+                        Icon(
+                            imageVector = destination.selectedIcon,
+                            contentDescription = null
+                        )
+                    } else {
+                        Icon(
+                            imageVector = destination.unselectedIcon,
+                            contentDescription = null
+                        )
+                    }
+                },
+                label = {
+                    Text(text = stringResource(id = destination.iconTextId))
+                })
+        }
+    }
+}
+
+@Composable
+fun NavController.currentDestination(): NavDestination? {
+    return currentBackStackEntryAsState().value?.destination
+}
+
+private fun NavController.navigateToTopLevelDestination(topLevelDestination: TopLevelDestination) {
+    val topLevelNavOptions = navOptions {
+        popUpTo(this@navigateToTopLevelDestination.graph.findStartDestination().id) {
+            saveState = true
+        }
+        launchSingleTop = true
+        restoreState = true
+    }
+
+    when (topLevelDestination) {
+        TopLevelDestination.NOW_PLAYING -> this.navigateToNowPlaying(topLevelNavOptions)
+        TopLevelDestination.FAVORITE -> this.navigateToFavorite(topLevelNavOptions)
+    }
+}
+
+private fun NavDestination?.isTopLevelDestinationInHierarchy(destination: TopLevelDestination) =
+    this?.hierarchy?.any {
+        it.route?.contains(destination.name, true) ?: false
+    } ?: false
